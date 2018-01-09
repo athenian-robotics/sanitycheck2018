dfb564a1a86b46f94933715cc70c0683bae2e700
ExtraE113 <33768950+ExtraE113@users.noreply.github.com>
2018-01-09 13:23:38 -0800

    Added more /gremlin messages

--- src/main/java/healthserver/Main.java
+++ src/main/java/healthserver/Main.java
@@ -1,12 +1,10 @@
 package healthserver;
 
-import healthserver.GetHealth;
-
 import static spark.Spark.*;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        String[] strs = new String[10];
+        String[] strs = new String[12];
         strs[0] = "Happy Gremlin and Sad Gremlin are arguing about the war...";
         strs[1] = "Gremlin down! Gremlin Down! Training new Gremlin...";
         strs[2] = "Extracting Gremlins from the pipeline...";
@@ -17,8 +15,10 @@ public class Main {
         strs[7] = "Declaring war on the Elvish kingdoms...";
         strs[8] = "Sending scouts through the tunnels...";
         strs[9] = "Building Gremlin Launchers...";
+        strs[10] = "Developing anit-Elf weapons...";
+        strs[11] = "Editing history to brainwash population...";
         Random randomGenerator = new Random();
-        get("/gremlin", (req, res) -> strs[randomGenerator.nextInt(10)]);
+        get("/gremlin", (req, res) -> strs[randomGenerator.nextInt(strs.length)]);
         String status = "TestFalse: " + GetHealth.checkStatus("TestFalse") + " | TestTrue: " + GetHealth.checkStatus("TestTrue");
         get("/status", (req, res) -> status);
     }
